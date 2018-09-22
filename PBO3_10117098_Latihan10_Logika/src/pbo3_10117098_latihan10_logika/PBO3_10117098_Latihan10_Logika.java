4358bfe2c765dd66478af04181c92723125a9296
Santi Susanti <43363256+saantisusanti@users.noreply.github.com>
2018-09-23 00:19:47 +0700

    Add files via upload

---
+++ PBO3_10117098_Latihan10_Logika/src/pbo3_10117098_latihan10_logika/PBO3_10117098_Latihan10_Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117098_latihan10_logika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Santi Susanti
+ * KELAS : PBO3
+ * NIM   : 10117098
+ */
+public class PBO3_10117098_Latihan10_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >=  17);
+        System.out.println("Status Dewasa : " +dewasa);
+    }
+    
+}
